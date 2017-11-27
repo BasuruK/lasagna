0af638a14a2c7bc46fa99f8ebc8effaa9c8b646b
Basuru Balasuriya <www.basururoxyouall@gmail.com>
2017-11-28 00:02:54 +0530

    more random stuff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import threading
+import subprocess
 from lasagna import RAG
 
 # Ram min config level in MB
@@ -8,11 +9,14 @@ mem_config_level_medium = 5000
 mem_status = RAG.GREEN
 
 # CPU Utilization config level
-cpu_config_level = None
+cpu_config_level_min = 10
+cpu_config_level_medium = 30
 cpu_status = RAG.GREEN
+
 # HDD Utilization config level
 hdd_config_level = None
 hdd_status = RAG.GREEN
+
 
 def check_ram_utilization_level():
     free_mem = os.popen('cat /proc/meminfo | grep "MemFree"')
@@ -31,10 +35,22 @@ def check_ram_utilization_level():
 
 
 def check_cpu_utilization_level():
-    print(None)
+    cpu_util = subprocess.Popen("mpstat | awk -F ' ' '{print $12}'", stdout=subprocess.PIPE, shell=True)
+    cpu_util = int(str(cpu_util.communicate()[0]).split("\\n")[3])
+
+    if cpu_util <= cpu_config_level_min:
+        return RAG.RED
+    elif cpu_config_level_medium <= cpu_util <= cpu_config_level_min:
+        return RAG.AMBER
+    elif cpu_util > cpu_config_level_medium:
+        return RAG.GREEN
+
+
+def check_hdd_utilization_level():
+    return True
 
 
 # Main functionality
 while True:
-
-
+    print(check_cpu_utilization_level())
+    break
